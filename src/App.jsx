c8e547d099d8dcb97d46698d486bf037ca203cb8
Parth-gothadiya <parthgothadiya@gmail.com>
2025-03-07 20:57:14 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
+// import { useState } from "react";
+// import Sidebar from "./components/Sidebar";
+// import Navbar from "./components/Navbar";
+// import RouterWrapper from "./routes";
+
+// function App() {
+
+//   return (
+//     <main>
+//       <Sidebar />
+//       <div className="main-content">
+//         <Navbar />
+//         <RouterWrapper/>
+//       </div>
+//     </main>
+//   );
+// }
+
+// export default App;
+
+
 import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import Navbar from "./components/Navbar";
 import RouterWrapper from "./routes";
+import ClickSpark from "./components/ClickSpark"; // Ensure correct path
 
 function App() {
-
   return (
-    <main>
-      <Sidebar />
-      <div className="main-content">
-        <Navbar />
-        <RouterWrapper/>
-      </div>
-    </main>
+    <ClickSpark sparkColor="orange" sparkSize={10} sparkRadius={15} sparkCount={8} duration={400}>
+      <main>
+        <Sidebar />
+        <div className="main-content">
+          <Navbar />
+          <RouterWrapper />
+        </div>
+      </main>
+    </ClickSpark>
   );
 }
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,22 +1,49 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import About from '../pages/About'
-import Contact from '../pages/Contact'
-import Portfolio from '../pages/Portfolio'
-import Resume from '../pages/Resume'
-import Service from '../pages/Service'
+// import React from 'react'
+// import { Route, Routes } from 'react-router-dom'
+// import About from '../pages/About'
+// import Contact from '../pages/Contact'
+// import Portfolio from '../pages/Portfolio'
+// import Resume from '../pages/Resume'
+// import Service from '../pages/Service'
+
+// const RouterWrapper = () => {
+//   return (
+//     <Routes>
+//       <Route path="/" element={<About />} />
+//       <Route path="/Service" element={<Service />} />
+//       <Route path="/contact" element={<Contact />} />
+//       <Route path="/portfolio" element={<Portfolio />} />
+//       <Route path="/resume" element={<Resume />} />
+//       <Route path="*" element={<About />} />
+//     </Routes>
+//   )
+// }
+
+// export default RouterWrapper
+
+
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Portfolio from "../pages/Portfolio";
+import Resume from "../pages/Resume";
+import Service from "../pages/Service";
+import ClickSpark from "../components/ClickSpark"; // Ensure the correct path
 
 const RouterWrapper = () => {
   return (
-    <Routes>
-      <Route path="/" element={<About />} />
-      <Route path="/Service" element={<Service />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/portfolio" element={<Portfolio />} />
-      <Route path="/resume" element={<Resume />} />
-      <Route path="*" element={<About />} />
-    </Routes>
-  )
-}
+    <ClickSpark sparkColor="orange" sparkSize={10} sparkRadius={15} sparkCount={8} duration={400}>
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/resume" element={<Resume />} />
+        <Route path="*" element={<About />} />
+      </Routes>
+    </ClickSpark>
+  );
+};
 
-export default RouterWrapper
+export default RouterWrapper;
