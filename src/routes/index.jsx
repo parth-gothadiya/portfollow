eaecd23354a006b9498bfb5cd7a13d8f0eb64196
Parth-gothadiya <parthgothadiya@gmail.com>
2025-04-25 22:36:03 +0530

    ok

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,25 +1,4 @@
-// import React from 'react'
-// import { Route, Routes } from 'react-router-dom'
-// import About from '../pages/About'
-// import Contact from '../pages/Contact'
-// import Portfolio from '../pages/Portfolio'
-// import Resume from '../pages/Resume'
-// import Service from '../pages/Service'
 
-// const RouterWrapper = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<About />} />
-//       <Route path="/Service" element={<Service />} />
-//       <Route path="/contact" element={<Contact />} />
-//       <Route path="/portfolio" element={<Portfolio />} />
-//       <Route path="/resume" element={<Resume />} />
-//       <Route path="*" element={<About />} />
-//     </Routes>
-//   )
-// }
-
-// export default RouterWrapper
 
 
 import React from "react";
